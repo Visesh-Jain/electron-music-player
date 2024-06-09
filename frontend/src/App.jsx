8d21e1301fa014b6f7eb4c5170af589f73b91b3f
Visesh <viseshjain.99@gmail.com>
2024-06-08 21:00:19 -0500

    package updates

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,5 @@
 import React from "react";
-import YTMusic from "ytmusic-api";
 
-const ytmusic = new YTMusic();
-await ytmusic.initialize(/* Optional: Custom cookies */);
-
-ytmusic.search("Never gonna give you up").then((songs) => {
-  console.log(songs);
-});
 function App() {
   return (
     <div>
